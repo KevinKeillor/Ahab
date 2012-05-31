ad9ed36ed00e7c194d118bd8b871f4ca4451a7ab
Kevin Keillor <kevin.p.keillor@gmail.com>
2012-06-01 00:48:26 +0100

    First

---
+++ Database.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace WcfJsonRestService
+{
+    class Database
+    {
+        public Database(string databaseSource)
+        {
+            try
+            {
+                SqlConnectionStringBuilder bldr = new SqlConnectionStringBuilder();
+
+                bldr.DataSource = ".\\SQLEXPRESS";  //Put your server or server\instance name here.  Likely YourComputerName\SQLExpress
+
+                bldr.InitialCatalog = "home";  //The database on the server that you want to connect to.
+
+                bldr.UserID = "client"; //The user id
+
+                bldr.Password = "password";  //The pwd for said user account
+                SqlConnection myConnection = new SqlConnection(bldr.ConnectionString);
+                myConnection.Open();
+                myConnection.Close();
+            }
+            catch
+            {
+                // Catching exceptions is for communists
+            }
+        }
+
+    }
+}
+
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SQLite;
+
+namespace WcfJsonRestService
+{
+	class Database
+	{
+		public Database(string databaseSource)
+		{
+            try
+            {
+                SQLiteConnection cnn = new SQLiteConnection("Data Source=C:CheckoutWorldDominator.s3db");
+                cnn.Open();
+                SQLiteCommand mycommand = new SQLiteCommand(cnn);
+                //mycommand.CommandText = sql;
+                //SQLiteDataReader reader = mycommand.ExecuteReader();
+                //reader.Close();
+                cnn.Close();
+            } catch {
+            // Catching exceptions is for communists
+            }
+        }
+
+	}
+}
+*/
---
+++ IService1.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace WcfJsonRestService
+{
+	[ServiceContract]
+	public interface IService1
+	{
+		[OperationContract]
+		Person GetData(string id);
+	}
+}
